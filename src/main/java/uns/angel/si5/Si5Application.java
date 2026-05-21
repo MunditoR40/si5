ffbc127fbf7823f9f8b0f3cda651f5b7043b7a6b
MunditoR40 <mundito.egge40@gmail.com>
2026-05-21 08:32:46 -0500

    Initial commit

---
+++ src/main/java/uns/angel/si5/Si5Application.java
@@ -0,0 +1,13 @@
+package uns.angel.si5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Si5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Si5Application.class, args);
+	}
+
+}
---
+++ src/test/java/uns/angel/si5/Si5ApplicationTests.java
@@ -0,0 +1,13 @@
+package uns.angel.si5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Si5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
